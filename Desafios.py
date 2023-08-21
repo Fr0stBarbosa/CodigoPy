54e42f3e44de0788c7518fe1140b67304cd7a8fc
Fr0stBarbosa <walterdesouzabarbosa@gmail.com>
2023-08-21 20:12:58 -0300

    Desafios resolvidos

--- Desafios.py
+++ Desafios.py
@@ -198,3 +198,15 @@ calculo = potencia(n1,n2)
 print(calculo)
 
 '''
+#desafio27
+
+def fatorial(n):
+    if n == 0:
+        return 1  # Caso base
+    else:
+        return n * fatorial(n - 1)  # Chamada recursiva
+
+numero = int(input('Digite um número para calcular o fatorial: '))
+resultado = fatorial(numero)
+print(f'O fatorial de {numero} é {resultado}')
+
