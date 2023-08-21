1774c507e25c29e7c88a897b5b3d4e698cb44f20
Fr0stBarbosa <walterdesouzabarbosa@gmail.com>
2023-08-21 19:49:37 -0300

    Aula 10 esta alterado

--- aula10.py
+++ aula10.py
@@ -1,6 +1,6 @@
 #Armazenar mais de uma informação em variaveis
 #manter a sequencia dos dados em uma variavel 
-#O lowe() metodo retorna uma string onde todos os 
+#O lower() metodo retorna uma string onde todos os 
 #caracteres sao minusculos
 #Simbolos e numeros sao ignorados
 '''
@@ -11,7 +11,7 @@ if cor_cliente.lower() in cores:
     print('sim')
 else:
     print('Não temos esta cor em estoque')
-'''
+
 #juntar listas
 
 cores = ['amarelo','verde','azul','vernelho','roxo']
@@ -20,3 +20,27 @@ valores=[10,20,30,40,50]
 duas_listas = zip(cores, valores)
 
 print(list(duas_listas))
+
+
+
+#Criar uma lista de frutas a apartir do input fornecido por virgulas:
+
+frutas_usuario = input('Digite o nome das frutas separados por virgula: ')
+
+frutas_lista = frutas_usuario.split(',')
+
+print(frutas_lista)
+'''
+
+#Tuples (Tuplas)
+#As tuplas são usadas para armazenar vários itens em uma unica variavel.
+cores_list = ['amarelo','verde','azul','vermelho']
+cores_tuples=('amarelo','verde','azul','vermelho')
+
+cores_list.append('roxo')
+
+print(type(cores_list))
+print(type(cores_tuples))
+
+duas_listas = cores_list * 2 
+print(duas_listas)
